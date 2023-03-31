6daafb653624d6fccd9f91dbd2a130cba1c32de8
Caro <carole.hug@outlook.com>
2023-03-31 14:00:36 +0200

    remove unused header content

--- src/services/likes.ts
+++ src/services/likes.ts
@@ -1,25 +1,21 @@
 import axios from 'axios';
 
 export const likePost = async (params?: { postId: string; likedByUser: boolean; accessToken?: string }) => {
-    const { postId, likedByUser, accessToken } = params || {};
+  const { postId, likedByUser, accessToken } = params || {};
 
-    if (!accessToken) {
-        throw new Error('No access token');
-    }
+  if (!accessToken) {
+    throw new Error('No access token');
+  }
 
-    const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}posts/${postId}/likes`;
+  const url = `${process.env.NEXT_PUBLIC_QWACKER_API_URL}posts/${postId}/likes`;
 
-    if (likedByUser) {
-        return await axios
-            .delete(url, { headers: { 'content-type': 'application/json', Authorization: `Bearer ${accessToken}` } })
-            .then((response) => {
-                console.log(response.data);
-            });
-    } else {
-        return await axios
-            .put(url, undefined, { headers: { 'content-type': 'application/json', Authorization: `Bearer ${accessToken}` } })
-            .then((response) => {
-                console.log(response.data);
-            });
-    }
+  if (likedByUser) {
+    return await axios.delete(url, { headers: { Authorization: `Bearer ${accessToken}` } }).then((response) => {
+      console.log(response.data);
+    });
+  } else {
+    return await axios.put(url, undefined, { headers: { Authorization: `Bearer ${accessToken}` } }).then((response) => {
+      console.log(response.data);
+    });
+  }
 };
