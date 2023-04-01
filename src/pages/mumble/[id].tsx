6048989df5257d5561aceccd2f13b8b0288a83b8
Caro <carole.hug@outlook.com>
2023-04-01 14:58:04 +0200

    add margin bottom

--- src/pages/mumble/[id].tsx
+++ src/pages/mumble/[id].tsx
@@ -37,7 +37,7 @@ export default function MumblePage({ mumble, replies }: Props): InferGetServerSi
 
   return (
     <>
-      <div className={'grid grid-cols-1 justify-items-center mt-m'}>
+      <div className={'grid grid-cols-1 justify-items-center my-m'}>
         <div className={'w-screen md:w-615'} s>
           <Card borderType={'rounded'} size={'M'}>
             <div className={'divide-y-1 divide-slate-200'}>
