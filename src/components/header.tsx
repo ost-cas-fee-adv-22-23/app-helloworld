4622d38237d1fe283604f6e63cfa40316a556bb6
Mehmet Ali Bekooglu <mehmetali.bekooglu@ost.ch>
2023-04-18 19:14:12 +0200

    fix: router and loading

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,16 +8,23 @@ import {
 } from '@smartive-education/design-system-component-library-hello-world-team';
 import { signOut, useSession } from 'next-auth/react';
 import { profileAvatar } from '../utils/profile-avatar';
+import { useRouter } from 'next/router';
 
 export const Header: FC = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const href = `/profile/me`;
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    router.push(href);
+  };
 
   return (
     <>
       {session && (
         <Navbar logoHref={'/'} logoAriaLabel={'Navigate to home'}>
           <span className={'absolute top-0'}>
-            <a href={`/profile/me`}>
+            <a href={href} onClick={(e) => handleClick(e)}>
               <ProfilePic
                 altText={'Profilbild'}
                 editLabel={'Bearbeiten'}
