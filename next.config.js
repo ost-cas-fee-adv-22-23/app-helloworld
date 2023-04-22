50bc2f80e39f9ab8cbba592adcc96554281e8c2b
Caro <carole.hug@outlook.com>
2023-04-22 15:03:50 +0200

    remove commented out text

--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = withPWA({
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     remotePatterns: [
-//       { protocol: 'https', hostname: 'cas-fee-advanced-ocvdad.zitadel.cloud' },
-//       {
-//         protocol: 'https',
-//         hostname: 'storage.googleapis.com',
-//         pathname: '/qwacker-api-prod-data/**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'picsum.photos',
-//       },
-//     ],
-//   },
-// });
-
 const withPWA = require('next-pwa')({
   dest: 'public',
 });
