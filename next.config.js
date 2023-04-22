79a66333464f8162dd4a57625eca44b448dc881c
Caro <carole.hug@outlook.com>
2023-04-22 15:02:15 +0200

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     remotePatterns: [
+//       { protocol: 'https', hostname: 'cas-fee-advanced-ocvdad.zitadel.cloud' },
+//       {
+//         protocol: 'https',
+//         hostname: 'storage.googleapis.com',
+//         pathname: '/qwacker-api-prod-data/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'picsum.photos',
+//       },
+//     ],
+//   },
+// });
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -16,6 +39,4 @@ const nextConfig = {
       },
     ],
   },
-};
-
-module.exports = nextConfig;
+})
