14a34a236c7e36672010eeb4e0575e0c7ce8aa10
Caro <carole.hug@outlook.com>
2023-06-23 23:12:35 +0200

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,6 +73,7 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
+      
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
