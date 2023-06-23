64733b0826c0040714926758e0c21dabd3c0d1b2
Caro <carole.hug@outlook.com>
2023-06-23 22:59:55 +0200

    fix reuseExistingServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -87,8 +87,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'next dev',
+    command: 'next build && next start',
     url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false,
   },
 });
